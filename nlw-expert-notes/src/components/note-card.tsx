6c4b8123711b13c9181adaa1f201bbf77b86cc12
Humberto Queiroz <90359980+HumbertoQueiroz@users.noreply.github.com>
2024-02-08 11:38:21 -0400

    add

--- nlw-expert-notes/src/components/note-card.tsx
+++ nlw-expert-notes/src/components/note-card.tsx
@@ -1,6 +1,9 @@
 // O '* as Dialog' importa tudo de dentro do diretorio passado no from e garda dentro de um objeto
 //Biblioteca Radix de componentes sem estilização
 import * as Dialog from '@radix-ui/react-dialog'
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { X } from 'lucide-react'
 
 
 interface NoteCardProps{
@@ -28,19 +31,29 @@ export function NoteCard({note}: NoteCardProps){
         </p>
         <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none'></div>
       </Dialog.Trigger>
-      
+        
       <Dialog.Portal>
         <Dialog.Overlay className='inset-0 fixed bg-black/50' />
-        <Dialog.Content className='z-10 bg-slate-700 rounded-md flex flex-col fixed left-1/2 -translate-x-1/2 -translate-y-1/2 top-1/2 max-w-[640px] w-full h-[60vh] outline-none '>
+        <Dialog.Content className='fixed overflow-hidden bg-slate-700 rounded-md flex flex-col left-1/2 -translate-x-1/2 -translate-y-1/2 top-1/2 max-w-[640px] w-full h-[60vh] outline-none '>
+          <Dialog.Close className=' absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100 rounded-md'>
+          <X className='size-5' />
+          </Dialog.Close>
           <div className='flex flex-1 flex-col gap-3 p-5'>
             <span className='text-sm font-medium text-slate-300'>
-              {note.date.toISOString()}
+              {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true})}
             </span>
             <div className='h-px w-full bg-slate-600/50'></div>
             <p className='text-sm leading-6 text-slate-400'>
               {note.content}
             </p>
           </div>
+
+          <button 
+            type="button" 
+            className='w-full bg-slate-800 py-4 text-center text-slate-300 outline-none font-medium group'
+          >
+            Deseja <span className='text-red-400 group-hover:underline'>apagar essa nota</span>?
+          </button>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
